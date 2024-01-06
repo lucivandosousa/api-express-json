b5a349e07bbe6889cd78473a2cb2d7158bfd8efc
Lucivando Sousa <lucivandosousa@gmail.com>
2024-01-06 16:38:11 -0300

    feat: adicionar biblioteca para trabalhar com variaveis de ambiente

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const { v4: uuidv4 } = require('uuid')
 const fs = require("fs")
+require("dotenv").config() 
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
